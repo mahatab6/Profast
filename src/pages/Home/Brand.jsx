758b5a668a88862c22dc614a251c1e3237e9827d
mahatab6 <mahatabcall56@gmail.com>
2025-06-20 17:36:34 +0600

    marquree added

---
+++ src/pages/Home/Brand.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import amazon from '../../assets/brands/amazon.png'
+import vector from '../../assets/brands/amazon_vector.png'
+import casio from '../../assets/brands/casio.png'
+import moonstar from '../../assets/brands/moonstar.png'
+import randstad from '../../assets/brands/randstad.png'
+import start from '../../assets/brands/start-people 1.png'
+import people from '../../assets/brands/start.png'
+import Marquee from 'react-fast-marquee';
+
+const Brand = () => {
+    const brandlogo = [ amazon, vector, casio, moonstar, randstad, start, people];
+
+    return (
+        <div className='py-22'>
+            <h1 className='text-3xl font-extrabold text-center py-5'>We've helped thousands of sales teams</h1>
+            <Marquee>
+                {
+                    brandlogo.map((logo) => (
+                        <div className='flex items-center mx-5'>
+                            <img src={logo} alt="" />
+                        </div>
+                    ))
+                }
+            </Marquee>
+        </div>
+    );
+};
+
+export default Brand;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './Hero';
 import Work from './Work';
 import Service from './Service';
+import Brand from './Brand';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Hero/>
             <Work/>
             <Service/>
+            <Brand/>
         </div>
     );
 };
